19b1eea30c884016baab72fe6e5d62ae10d89a8b
alfred <Game_Alfred_Boekhoudt@hotmail.com>
2017-11-14 17:20:11 -0400

    Create followers.js

---
+++ events/followers.js
@@ -0,0 +1,60 @@
+
+const radelf = require("../radelf-client");
+const radelfFunction = require("../radelf-functions");
+const secrets = require("../config/secrets");
+
+var newFollows = [];
+
+var latestFollow = "#";
+
+setInterval(function(){
+
+    if(radelf.broadcasterID == "#"){
+        console.log("radelf's broadcasterID is: " + radelf.broadcasterID);
+        radelf.api(radelfFunction.queryTwitchAPI(
+            "users?login=alfred1203"
+        ), function(err, res, body){
+            radelf.broadcasterID = body.users[0]._id;
+            console.log("This is our channel id: " + radelf.broadcasterID);
+            checkFollows(radelf.broadcasterID);
+        });
+    } else {
+        checkFollows(radelf.broadcasterID);
+    }
+    
+    if (newFollows.length > 0) radelf.emit("newFollow", newFollows.shift());
+}, 1000 * 10); // 1sec * 10 = 10sec timer
+
+
+
+function checkFollows(id){
+    let newFollowsHold = [];
+    
+    radelf.api(radelfFunction.queryTwitchAPI(
+        "channels/" + id + "/follows/"
+    ), function(err, res, body) {
+        if(err || body.follows == null || "error" in body) {
+            console.error("There was a problem querying the Twitch API for follows:");
+            console.error(err);
+            
+            console.warn("There was no follows array returned in the Twitch API response.");
+            if(body.error) console.error(body.error + ": " + body.message);
+            return;
+        } else{
+            if (latestFollow == "#") {
+                latestFollow = body.follows[0].user.display_name;
+            } else {
+                for( let i = 0; i < body.follows.length; i++){
+                    if(body.follows[i].user.display_name != latestFollow){
+                        newFollowsHold.push(body.follows[i].user.display_name);
+                    } else{
+                        latestFollow = body.follows[0].user.display_name;
+                        newFollows = newFollows.concat(newFollowsHold);
+                        break;
+                    }
+                }
+                latestFollow = body.follows[0].user.display_name;
+            }
+        }
+    });    
+}
