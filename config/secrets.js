c9b78317e1271270fd8e1ce51cf1d5b74f402600
alfred <Game_Alfred_Boekhoudt@hotmail.com>
2017-11-14 17:23:54 -0400

    Update secrets.js

--- config/secrets.js
+++ config/secrets.js
@@ -1,14 +1,14 @@
 module.exports = {
     broadcaster: {
         username: "alfred1203",
-        password: "oauth:aqazz1e4e1ufxh61il5cc16r9vjbzn"
+        password: "oauth:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
     }, 
     radelfbot: {
         username: "radelfbot",  
-        password: "oauth:kmhf0lf4xi2bj6i5gf7sy9sypqpc6m"
+        password: "oauth:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
     },
-    clientId: "q6batx0epp608isickayubi39itsckt",
+    clientId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
     discord: {
-        token: "MzU3MzQwNDM4NDIxNTY5NTM2.DM_E-w.XBCAAR_z1tHO9tAFwdWRGWr_xZw"
+        token: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
     }
 }
